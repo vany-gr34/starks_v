b484ed4508ac241267316a5b048b7286255717b8
vany-gr34 <vanye25@yahoo.com>
2026-01-28 10:03:25 +0100

    project setup and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup 
+setup(
+    name="starks v0.1",
+    version="0.0.0",
+    author="kaouthar hamdan ",
+    author_email="kaoutarhamdan222@hmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
