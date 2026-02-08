2c80526a0fc65d320a32a282783e4328932953ed
vany-gr34 <vanye25@yahoo.com>
2026-02-08 13:18:11 +0100

    fixed data path and tested the training processs

--- eaglescan/constant/training_pipeline/__init__.py
+++ eaglescan/constant/training_pipeline/__init__.py
@@ -7,7 +7,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1GSkVXhCP4hS32xRE0MVBrq4mfO7hG6yx/view?usp=sharing"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1-gOZflMhmO-bGgU6NoIYX2mEIk7eGpV3/view?usp=sharing"
 
 
 
